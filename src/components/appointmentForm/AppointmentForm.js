34cfe5e21736095544331d2beeacfbeb6dd8439d
Kurt <kurtgreen85@gmail.com>
2023-02-12 10:56:38 -0600

    completed through step 8

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,52 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input  
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            placeholder="Appointment Title"
+          />
+        </label>
+        <br />
+        <label>
+          <input 
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            min={getTodayString()}
+          />
+        </label>
+        <br />
+        <label>
+          <input 
+            type="time"
+            name="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <lable>
+          <ContactPicker 
+            name="contacts"
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
+            placeholder="Appointment With"
+            contacts={contacts}
+          />
+        </lable>
+        <br />
+        <input type="submit" value="Add Appointment" />
+      </form>
+    </>
   );
 };
